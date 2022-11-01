47ceb53fd68059748e0464b4ea7530717a725e2d
v08uka <v08uka@gmail.com>
2022-11-01 19:38:34 +0200

    Added a new controller

---
+++ Dotnet-RPG/Controllers/CharacterController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Dotnet_RPG.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class CharacterController : ControllerBase
+{
+    private static List<Characer> craracters = new List<Characer>()
+    {
+        new Characer(),
+        new Characer {Id = 1, Name = "Sam"}
+    };
+
+    [HttpGet("GetAll")]
+    public ActionResult<List<Characer>> Get()
+    {
+        return Ok(craracters);
+    }
+    
+    [HttpGet("{id:int}")]
+    public ActionResult<Characer> GetSingle(int id)
+    {
+        return Ok(craracters.FirstOrDefault(c => c.Id == id));
+    }
+
+    [HttpPost]
+    public ActionResult<List<Characer>> AddCharacter(Characer newCharacter)
+    {
+        craracters.Add(newCharacter);
+        return Ok(craracters);
+    }
+}
--- Dotnet-RPG/Program.cs
+++ Dotnet-RPG/Program.cs
@@ -1,3 +1,5 @@
+global using Dotnet_RPG.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
